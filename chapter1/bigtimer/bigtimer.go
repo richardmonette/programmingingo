186e738ace77de7500c005da0ac1ee4bc8ce7126
Richard Monette <richard.monette@gmail.com>
2013-05-09 22:34:53 -0700

    Updating with some array and slices self study

--- chapter1/bigtimer/bigtimer.go
+++ chapter1/bigtimer/bigtimer.go
@@ -5,16 +5,46 @@ import (
 	"io/ioutil"
 	"strings"
 	"fmt"
+	//"strconv"
 )
 
 func main() {
-	var bigDigits [7][10]string
-	
+
 	content, err := ioutil.ReadFile("digits.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	stringcontent := string(content)
-	lines := strings.Split(stringcontent, "\n")
-	fmt.Println(lines)
+	lines := strings.Split(stringcontent, "\r\n")
+	
+	var digits [70][7]string
+	
+	for i, line := range lines {
+		for x := 0; x < 7; x++ {
+			digits[i][x] = string(line[x])
+			//fmt.Print(string(line[x]))
+		}
+		//fmt.Print("\n")
+	}
+	
+	toDraw := []int{1, 3, 3, 7}
+
+	for x := 0; x < 7; x++ {
+		for _, value := range toDraw {
+			line := digits[value * 7 + x]
+			for _, char := range line {
+				fmt.Print(char)
+			}
+		}
+		fmt.Print("\n")
+	}
+	
+	/*for i, _ := range lines {
+		for x := 0; x < 7; x++ {
+			fmt.Print(digits[i][x])
+		}
+		fmt.Print("\n")
+	}*/
+
 }
